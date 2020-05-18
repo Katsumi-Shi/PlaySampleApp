141aea5b4741a16fea395120aa2e3f7427dd1884
Katsumi-Shi <s-katsumi@zerotoone.co.jp>
2020-05-18 18:02:03 +0900

    create delete method & routes update

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -8,14 +8,16 @@ import play.data.Form;
 import play.data.FormFactory;
 import tables.T_User;
 import tables.FormTable;
-import io.ebean.Finder;
-import play.db.ebean.Transactional;
+import io.ebean.*;
+import play.db.ebean.*;
+
 
 @Singleton
 public class UserController extends Controller{
    private Form<T_User> form;
    private Form<FormTable> form2;
    private FormFactory formFactory;
+   T_User tuser = new T_User();
 
    @Inject
    public UserController(FormFactory formFactory) {
@@ -23,7 +25,7 @@ public class UserController extends Controller{
        this.form2 = formFactory.form(FormTable.class);
    }
    public Result init() {
-       return ok(views.html.user.render(false));
+       return ok(views.html.user.render(tuser, false));
    }
    
    @Transactional
@@ -37,37 +39,29 @@ public class UserController extends Controller{
        user2.height = user.height;
        user2.food = user.food;
         user2.save();
-       return ok(views.html.user.render(false));
+       return ok(views.html.user.render(tuser, false));
    }
    
    @Transactional
-   
    public Result edit(Long id) {
 	   Finder<Long, T_User> finder = new Finder<Long, T_User>(T_User.class);
-       T_User user = finder.byId(id);
-       System.out.println(user);
-       return ok(views.html.user.render(user, true));
+       T_User newuser = finder.byId(id);
+       return ok(views.html.user.render(newuser, true));
    }
    
    @Transactional
-   
    public Result update(Http.Request rq, Long id) {
 	   Finder<Long, T_User> finder = new Finder<Long, T_User>(T_User.class);
-       T_User iduser = finder.byId(id);
+       T_User user2 = finder.byId(id);
 	   Form<FormTable> userForm = form2.bindFromRequest(rq);
        FormTable user = userForm.get();
-       T_User user2 = new T_User();
-       System.out.println(user2);
-       //user2.id = iduser.id;
        user2.name = user.name;
-       System.out.println(user2.name);
-       System.out.println(iduser.id);
        user2.schoolYear = user.schoolYear;
        user2.birthDay = user.birthDay;
        user2.height = user.height;
        user2.food = user.food;
 	   user2.update();
-       return ok(views.html.user.render(true));
+       return ok(views.html.user.render(user2, true));
    }
    
    @Transactional
