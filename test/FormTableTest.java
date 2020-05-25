afdf76476f5d5fa8dce35b98dc5b55d3f1c3d6d6
Katsumi-Shi <s-katsumi@zerotoone.co.jp>
2020-05-25 14:12:30 +0900

    add modelTest1

---
+++ test/FormTableTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import tables.FormTable;
+
+public class FormTableTest {
+
+    @Test
+    public void formtableTest() {
+    FormTable user = new FormTable();
+    user.name = "Sample1";
+    assertEquals("Sample1", user.name);
+}
+    
+}
